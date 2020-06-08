2cfc5e7ce573f66273fbe20216872b527febad76
yunyajie <1545357935@qq.com>
2020-06-08 20:02:32 +0800

    添加App图标

--- app/src/main/java/com/example/vedit/Activities/ActionsActivity.java
+++ app/src/main/java/com/example/vedit/Activities/ActionsActivity.java
@@ -1,8 +1,10 @@
 package com.example.vedit.Activities;
 
 import android.content.Intent;
+import android.media.MediaScannerConnection;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
